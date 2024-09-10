9e46424ed6276901fffe0453d8ee4e91da14c43f
Bryon Bailey <bryon.r.bailey@gmail.com>
2024-09-10 12:10:20 -0400

    Add project files.

---
+++ RadFixes/Patches.cs
@@ -0,0 +1,63 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using System.Linq;
+
+namespace RadFixes
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(ShipItemFishingRod))]
+        private class ShipItemFisingRodPatches
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch("OnLoad")]
+            public static void OnLoadSetReverseSpinner(ref float ___spinnerSpeed)
+            {
+                ___spinnerSpeed = -40f;
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch("OnBuy")]
+            public static void OnBuySetReverseSpinner(ref float ___spinnerSpeed)
+            {
+                ___spinnerSpeed = -40f;
+            }
+        }
+
+        [HarmonyPatch(typeof(StartMenu))]
+        private class StartMenuPatches
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch("Awake")]
+            public static void DestroyDeco(GameObject ___settingsUI)
+            {
+                var deco = ___settingsUI.GetComponentsInChildren<Transform>().FirstOrDefault(k => k.name == "deco (1)");
+                Object.Destroy(deco.gameObject);
+            }
+        }
+
+        [HarmonyPatch(typeof(BackupSavesListUI))]
+        private class BackupSavesListUIPatches
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch("Awake")]
+            public static void SetShowingListFor(ref int ___showingListFor)
+            {
+                ___showingListFor = -1;
+            }
+        }
+
+        [HarmonyPatch(typeof(MouseButtonPointer))]
+        private class MouseButtonPointerPatches
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("LateUpdate")]
+            public static bool DisableMouseInputLostFocus()
+            {
+                if (!Application.isFocused)
+                    return false;
+                return true;
+            }
+        }
+    }
+}
---
+++ RadFixes/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace RadFixes
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "com.raddude82.radfixes";
+        public const string PLUGIN_NAME = "RadFixes";
+        public const string PLUGIN_VERSION = "1.0.0";
+
+        internal static Plugin instance;
+        internal static ManualLogSource logger;
+
+        private void Awake()
+        {
+            instance = this;
+            logger = Logger;
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PLUGIN_GUID);
+        }
+    }
+}
